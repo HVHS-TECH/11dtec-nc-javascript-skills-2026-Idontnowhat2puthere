4591e4d4e819bc1aedcaf35900547ead3eaacf2d
Idontnowhat2puthere <24248jb@hvhs.school.nz>
2026-04-29 02:48:32 +0000

    Apr 29, 2026, 2:48 AM

--- t02_variables.js
+++ t02_variables.js
@@ -6,7 +6,7 @@ console.log("Running t01_introduction.js")
 console.log("hello")
 
 //varibales
-let Username = "James";
+let username = "James";
 let year = 2026;
 let age = 15;
 let money = 255;
@@ -15,7 +15,7 @@ let money = 255;
 /****************************
  Main code
  ****************************/
-console.log("Hi "+ Username + " as of "+ year +" you are " + age + " years old. You have " + money + " dollars")
+console.log("Hi "+ username + " as of "+ year +" you are " + age + " years old. You have " + money + " dollars")
 
 
 /****************************
--- t03_maths.js
+++ t03_maths.js
@@ -5,16 +5,21 @@ console.log("Running t01_introduction.js")
 console.log("hello")
 
 //varibales
-let Username = "James";
+let username = "James";
 let year = 2026;
 let age = 15;
 let money = 255;
+let birthYear = 2011;
+let oldAge = ;
+oldAge = age + 10;
+birthYear = year - age;
 
 
 /****************************
  Main code
  ****************************/
-console.log("Hi "+ Username + " as of "+ year +" you are " + age + " years old. You have " + money + " dollars")
+console.log("Hi "+ username + " as of "+ year +" you are " + age + " years old. You were born in " + money + " dollars")
+console.log("You were born in " + birthYear)
 
 
 /****************************
