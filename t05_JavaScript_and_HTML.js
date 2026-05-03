f044e334c3ad77a657f9c830afca4347b741397f
Idontnowhat2puthere <24248jb@hvhs.school.nz>
2026-05-03 22:23:39 +0000

    May 3, 2026, 10:23 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,34 @@
-/* Paste your code from task 3, Maths here */
+/****************************
+Name of task: Maths
+****************************/
+console.log("Running t01_introduction.js")
+console.log("hello")
+
+//varibales
+let username = "James";
+let year = 2026;
+let age = 15;
+let money = 255;
+let birthYear = 2011;
+let oldAge;
+oldAge = age + 10;
+birthYear = year - age;
+money = money / 2; 
+halfMoney = money / 2;
+
+
+/****************************
+ Main code
+ ****************************/
+console.log("Hi "+ username + " as of "+ year +" you are " + age + " years old. You were born in " + birthYear)
+console.log("In 10 years you'll be " + oldAge + "year old ")
+console.log(" You have " + money + " dollars ")
+console.log("You spend half of your money, now you have " + halfMoney)
+console.log("Then you get $3, now you have" + (halfMoney + 3) + "dollars")
+
+
+/****************************
+ functions
+ ****************************/
+
+// This is a single line comment/
