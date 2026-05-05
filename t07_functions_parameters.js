553923aaedb3c6a1ec82c098d239ddddf7eca74b
Idontnowhat2puthere <24248jb@hvhs.school.nz>
2026-05-05 04:56:14 +0000

    May 5, 2026, 4:56 AM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -1,1 +1,48 @@
-/* Paste your code from task 6 here */
+/****************************
+Name of task: Javascript and HTML
+****************************/
+console.log("Running t07_functions_parameters.js")
+
+//varibales
+let username = "James";
+let year = 2026;
+let age = 15;
+let money = 255;
+let birthYear = 2011;
+let oldAge;
+oldAge = age + 10;
+birthYear = year - age;
+money = money / 2; 
+halfMoney = money / 2;
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>"; 
+
+/****************************
+ Main code
+ ****************************/
+
+OUTPUT.innerHTML += ("<p>Hi "+ username + " as of "+ year +" you are " + age + " years old.</p>") 
+OUTPUT.innerHTML += ("<p>You were born in " + birthYear)
+OUTPUT.innerHTML += ("<p>In 10 years you'll be " + oldAge + " year old</p>")
+OUTPUT.innerHTML += ("<p>You have " + money + " dollars</p>")
+OUTPUT.innerHTML += ("<p>You spend half of your money, now you have " + halfMoney + " dollars</p>")
+OUTPUT.innerHTML += ("<p>Then you get $3, now you have " + (halfMoney + 3) + " dollars</p>")
+
+
+/****************************
+ functions
+ ****************************/
+function Welcome(){
+OUTPUT.innerHTML += "<p>Welcome</p>";
+}
+
+Welcome(); 
+
+function displayWelcome(_name, _age){
+    OUTPUT.innerHTML += "<p>Welcome to the page " +_name+"</p>";
+    OUTPUT.innerHTML += "<p>You are " +_age+" years old</p>";
+}
+
+displayWelcome();
+
+// This is a single line comment/
