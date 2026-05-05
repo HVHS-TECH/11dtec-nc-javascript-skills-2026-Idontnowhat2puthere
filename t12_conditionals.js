f49542c77e217ff21fd9e6872fa3bfd1cebdbda9
Idontnowhat2puthere <24248jb@hvhs.school.nz>
2026-05-05 20:32:51 +0000

    May 5, 2026, 8:32 PM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,1 +1,59 @@
-/* Paste your code from the last task */
+console.log("Running t12_conditionals.js")
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+function start() {
+
+//get value from input box
+let username = document.getElementById("usernameInput").value; 
+let age = number(document.getElementById("ageInput").value); 
+let money = number(document.getElementById("moneyInput").value); 
+
+
+
+//varibales
+let year = 2026;
+let birthYear = 2011;
+let oldAge;
+oldAge = age + 10;
+birthYear = year - age;
+money = money / 2; 
+let halfMoney = money / 2;
+
+
+//clear output (important)
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>"; 
+
+
+/****************************
+ Main code
+ ****************************/
+
+OUTPUT.innerHTML += ("<p>Hi "+ username + " as of "+ year +" you are " + age + " years old.</p>") 
+OUTPUT.innerHTML += ("<p>You were born in " + birthYear)
+OUTPUT.innerHTML += ("<p>In 10 years you'll be " + oldAge + "year old</p>")
+OUTPUT.innerHTML += ("<p>You have " + money + " dollars</p>")
+OUTPUT.innerHTML += ("<p>You spend half of your money, now you have " + halfMoney + " dollars</p>")
+OUTPUT.innerHTML += ("<p>Then you get $3, now you have" + (halfMoney + 3) + "dollars</p>")
+
+
+/****************************
+ functions
+ ****************************/
+
+Welcome(); 
+
+}
+
+function Welcome(){
+OUTPUT.innerHTML += "<p>Welcome</p>";
+}
+
+function start() {
+    spaceForJavaScriptOutput.innerHTML
+    spaceForJavaScriptOutput.innerHTML
+    answer = currentYear - userAge;
+    spaceForJavaScriptOutput.innerHTML
+}
+
+// This is a single line comment/
