20f1b5b62c32f543b669b8718935a2ba1379b048
Idontnowhat2puthere <24248jb@hvhs.school.nz>
2026-05-13 06:13:03 +0000

    May 13, 2026, 6:13 AM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -1,1 +1,60 @@
-/* Paste your code from the last task */
+console.log("Running t13_functions_return.js")
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+
+//get value from input box
+let username = document.getElementById("usernameInput").value; 
+let age = Number(document.getElementById("ageInput").value); 
+let money = Number(document.getElementById("moneyInput").value); 
+let year = 2026;
+
+
+//varibales
+let oldAge;
+oldAge = age + 10;
+let = birthYear = year - age;
+
+
+
+//clear output (important)
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>"; 
+
+
+/****************************
+ Main code
+ ****************************/
+
+OUTPUT.innerHTML += ("<p>Hi "+ username + " as of "+ year +" you are " + age + " years old.</p>") 
+OUTPUT.innerHTML += ("<p>You were born in " + birthYear)
+OUTPUT.innerHTML += ("<p>In 10 years you'll be " + oldAge + "year old</p>")
+OUTPUT.innerHTML += ("<p>You have " + money + " dollars</p>")
+OUTPUT.innerHTML += ("<p>You spend half of your money, now you have " + halfMoney + " dollars</p>")
+OUTPUT.innerHTML += ("<p>The current year is" + year)
+OUTPUT.innerHTML += ("<p>A chocolate bar costs $4</p>")
+
+/****************************
+ functions
+ ****************************/
+
+Welcome(); 
+
+
+function Welcome(){
+OUTPUT.innerHTML += "<p>A chocolate bar costs $4</p>";
+}
+
+if (money < 4){
+    console.log("You're broke")
+}
+
+Welcome(); 
+
+function start() {
+    spaceForJavaScriptOutput.innerHTML
+    spaceForJavaScriptOutput.innerHTML
+    answer = year - userAge;
+    spaceForJavaScriptOutput.innerHTML
+}
+
+// This is a single line comment/
